659ba02eab6abaf17ad1d2cf0c3eb0f0f4400326
abdielrumaldom <abdielrumaldom@gmail.com>
2020-08-31 18:52:38 -0500

    Security Update

--- App/App/settings.py
+++ App/App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=6=^a)7u7%n$zv4)a40pm$c#gl=__g&v4ogz^ga4!h8(3hggry'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
